57eeb2ea959c5bd81e53b46e6035349f35ffe1cb
Mada Aryakusumah <lokermada@gmail.com>
2021-04-04 08:01:21 +0700

    handle nil for support pagination

--- lib/administrate/field/nested_has_many.rb
+++ lib/administrate/field/nested_has_many.rb
@@ -32,7 +32,7 @@ module Administrate
           next if nested_field.resource.blank?
 
           # inject current data into field
-          resource = data[form_builder.index]
+          resource = data[form_builder.index] || form_builder.object
           nested_field.instance_variable_set(
             "@data",
             resource.send(nested_field.attribute),
